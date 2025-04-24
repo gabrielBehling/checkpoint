f8fd8e0f6c8bad2887320f097d35b2e6fafba3f0
gabrielBehling <gabrielbehling1@gmail.com>
2025-04-24 20:00:33 -0300

    Fix some bugs in the containers

--- microservices/chat-service/src/app.js
+++ microservices/chat-service/src/app.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const app = express();
 
-// Status endpoint
-app.get('/status', (req, res) => {
+app.get('/health', (req, res) => {
     res.json({ status: 'OK', timestamp: new Date().toISOString() });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Chat Service is running on port ${PORT}`);
