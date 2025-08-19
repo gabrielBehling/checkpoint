2b8c6e2369c2c6a7781c3362bf43e881c909de2c
Adryel115 <adryel5estrela@gmail.com>
2025-08-19 11:11:04 -0300

    Cria sistema de sanitização dos inputs.

--- microservices/chat-service/src/app.js
+++ microservices/chat-service/src/app.js
@@ -1,50 +1,45 @@
-const { Socket } = require('dgram');
-const { renderFile } = require('ejs');
 const express = require('express');
+const path = require('path');
+const { renderFile } = require('ejs');
+const http = require('http');
+const socketIO = require('socket.io');
+const validator = require('validator');
 
-const path = require('path')
 const app = express();
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, '../Implementacao_Socket_Teste')))
-app.set('views', path.join(__dirname, '../Implementacao_Socket_Teste'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+
+
+app.use(express.static(path.join(__dirname, '../Implementacao_Socket_Teste')));
+app.set('views', path.join(__dirname, '../Implementacao_Socket_Teste'));
+app.engine('html', renderFile);
+app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
-    res.render('index.html')
-})
+    res.render('index.html');
+});
 
-let messages = []
+let messages = [];
 
 io.on('connection', socket => {
-    console.log(`Socket connected:  ${socket.id}`)
+    console.log(`Socket connected: ${socket.id}`);
 
     socket.on('sendMessage', data => {
-        messages.push(data)
-        socket.broadcast.emit('receivedMessage', data)
-    })
-})
+        
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+});
 
-server.listen(3000)
-
-
-
-
-
-
-
-
-
-
-
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
 
 app.get('/health', (req, res) => {
     res.json({ status: 'OK', timestamp: new Date().toISOString() });
-
-    
 });
+
 
 // app.get('/', (req, res) => {
 //     res.json({
