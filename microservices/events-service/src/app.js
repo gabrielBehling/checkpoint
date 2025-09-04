376fa1bbc8702b7a5cfc94ecc2b277fe6efbe55b
gabrielBehling <gabrielbehling1@gmail.com>
2025-09-04 11:53:12 -0300

    WIP: Start events service

--- microservices/events-service/src/app.js
+++ microservices/events-service/src/app.js
@@ -1,6 +1,35 @@
 const express = require('express');
+const sql = require('mssql');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { object, string, date } = require('yup');
+
+const dbConfig = {
+    server: process.env.MSSQL_SERVER,
+    port: parseInt(process.env.MSSQL_PORT),
+    database: process.env.MSSQL_DATABASE,
+    user: process.env.MSSQL_USER,
+    password: process.env.MSSQL_PASSWORD,
+    options: {
+        trustServerCertificate: process.env.MSSQL_TRUST_SERVER_CERTIFICATE === 'True',
+        encrypt: true
+    }
+};
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+let createEventSchema = object({
+    title: string().required(),
+    Description: string().required(),
+    StartDate: date(),
+    // ...
+});
+app.post('/', (req, res) => {
+
+});
 
 app.get('/health', (req, res) => {
     res.json({ status: 'OK', timestamp: new Date().toISOString() });
