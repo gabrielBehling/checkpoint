3f16c42479e73a600c849a4429b28b85882b957b
gabrielBehling <gabrielbehling1@gmail.com>
2025-09-09 21:53:21 -0300

    feat(events-service): add event creation and authentication middleware
    - Updated package.json to include new dependencies: cookie-parser, jsonwebtoken, mssql, and yup.
    - Implemented authMiddleware for JWT authentication in app.js.
    - Enhanced event creation endpoint to validate input using Yup and insert event data into the database.
    - Added a new GET endpoint to retrieve events.
    - Improved error handling for event creation.

--- microservices/events-service/src/app.js
+++ microservices/events-service/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const sql = require('mssql');
-const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { object, string, date } = require('yup');
+const { object, string, date, number } = require('yup');
+const authMiddleware = require('./authMiddleware');
 
 const dbConfig = {
     server: process.env.MSSQL_SERVER,
@@ -22,14 +22,44 @@ app.use(express.json());
 app.use(cookieParser());
 
 let createEventSchema = object({
-    title: string().required(),
+    Title: string().required(),
     Description: string().required(),
-    StartDate: date(),
-    // ...
+    StartDate: date().required(),
+    EndDate: date().required(),
+    Location: string(),
+    MaxParticipants: number(),
+    MaxTeams: number(),
+    Rules: string(),
+    Prizes: string(),
+    BannerURL: string().url()
 });
-app.post('/', (req, res) => {
+app.post('/', authMiddleware, async (req, res) => {
+    try {
+        const eventData = await createEventSchema.validate(req.body);
+        
+        const userId = req.user.userId;
 
+        await sql.connect(dbConfig);
+        const result = await sql.query`
+            INSERT INTO Events (Title, Description, StartDate, EndDate, Location, MaxParticipants, MaxTeams, Rules, Prizes, BannerURL, CreatedBy)
+            values (${eventData.Title}, ${eventData.Description}, ${eventData.StartDate}, ${eventData.EndDate}, ${eventData.Location || null}, 
+                    ${eventData.MaxParticipants || null}, ${eventData.MaxTeams || null}, ${eventData.Rules || null}, 
+                    ${eventData.Prizes || null}, ${eventData.BannerURL || null}, ${userId});
+        `;
+        
+        
+        res.status(201).json({ message: 'Event created successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+    finally {
+        await sql.close();
+    }
 });
+
+app.get('/', authMiddleware, async (req, res) => {
+    res.status(200).json({ message: 'Events' });
+})
 
 app.get('/health', (req, res) => {
     res.json({ status: 'OK', timestamp: new Date().toISOString() });
